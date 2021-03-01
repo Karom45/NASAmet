b27fd017d46a01bd495b2f923a013eadf43dacdf
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-03-02 01:10:00 +0300

    вся верстка кроме формы регистрации

--- NASAmet/apps/accounts/views.py
+++ NASAmet/apps/accounts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.shortcuts import redirect
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+def redirect_view(request):
+    return redirect('/meteorites/meteorites_list')
